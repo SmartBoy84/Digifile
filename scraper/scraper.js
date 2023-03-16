c211d1de06166359df2c80a694285a9925cb5244
SmartBoy84 <www.hamdanmahmood12@gmail.com>
2023-03-16 19:42:06 +0800

    final fix - completely harness JS quirks

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,9 +1,5 @@
 let getWait = (d) => new Promise(resolve => setTimeout(resolve, d))
 
-// flags because extensisons' addListener() is pedantic
-let failure = {}
-let success = []
-let currentlyRunning = 0
 let scraping = false
 
 let alertBridge = (str) => chrome.runtime.sendMessage({ "type": "alert", "message": str })
@@ -11,57 +7,58 @@ let alertBridge = (str) => chrome.runtime.sendMessage({ "type": "alert", "messag
 chrome.runtime.onMessage.addListener(async (request, sender, reply) => {
 
     if (request["type"] == "contents") {
-
-        let contents = request["contents"]
-
-        if (request["history"]) {
-            success = request["history"]
-
-            console.log("Entries before: ", contents.length)
-            contents = contents.filter(a => !success.some(b => a[0] == b))
-            console.log("Entries after: ", contents.length)
-        }
-
-        scrape(contents)
+        scrape(request["contents"], request["history"])
     }
 
     if (request["type"] == "pause") {
         scraping = false
     }
 
-    if (request["type"] == "error") { // must listen for reply here to avoid race condition
-        console.log("got reply from", request["name"])
+})
 
-        if (request["type"] == "error") {
-            if (request["error"] && request["error"].length > 0) {
-                failure[request["name"]] = request["error"]
-                console.log("error: ", request["error"])
+let waitForResponse = (cFn, ...idList) =>
+    new Promise((resolve, reject) => {
 
-                reply({ "received": true })
+        let error = (str) => resolve({ "failed": true, "error": str })
 
-            } else {
-                success.push(request["name"])
+        let tabRemoval = function (tabId, removeInfo) {
+            if (idList.includes(tabId)) {
+                console.log("tab closed!", tabId)
+
+                chrome.tabs.onRemoved.removeListener(arguments.callee)
+                error("tab closed")
             }
         }
 
-        currentlyRunning-- // finished!
-        return true
-    }
-})
+        let messageReceived = function (request, sender, reply) {
 
-let waitForResponse = async (id, cFn) =>
-    await new Promise((resolve, reject) => {
-        chrome.runtime.onMessage.addListener(function (request, sender, reply) {
-
-            if (sender.tab && sender.tab.id == id) {
-                if (!cFn || (cFn && cFn(request, reply))) { // only stop listening if a callback function is defined and returns true or if it isn't defined
+            if (sender.tab && idList.includes(sender.tab.id)) {
+                if (!cFn || (cFn && cFn(request, sender, reply))) { // only stop listening if a callback function is defined and returns true or if it isn't defined
 
                     console.log("successfully exchanged message")
-                    resolve(request)
+
+                    chrome.tabs.onRemoved.removeListener(tabRemoval)
                     chrome.runtime.onMessage.removeListener(arguments.callee)
+
+                    resolve(request)
                 }
             }
-        })
+        }
+
+        if (idList) {
+            for (let id of idList) { // ensure that tab exists
+
+                chrome.tabs.get(id, function () {
+                    if (chrome.runtime.lastError) {
+
+                        console.log("tab doesn't exist!", chrome.runtime.lastError.message)
+                        error("tab doesn't exist")
+                    }
+                })
+                chrome.tabs.onRemoved.addListener(tabRemoval)
+            }
+        }
+        chrome.runtime.onMessage.addListener(messageReceived)
     })
 
 let saveData = async (fileName, data) => {
@@ -69,7 +66,7 @@ let saveData = async (fileName, data) => {
     console.log("Saving data...")
     let id = (await chrome.tabs.create({ url: chrome.runtime.getURL("reporter/reporter.html") })).id
 
-    await waitForResponse(id, (request, reply) => {
+    await waitForResponse((request, sender, reply) => {
         if (request["type"] == "downloader") {
             console.log("sending data to download...")
 
@@ -77,76 +74,111 @@ let saveData = async (fileName, data) => {
             return true
         }
         return false
-    })
+    }, id)
 
-    await waitForResponse(id, (request, reply) => request["type"] == "downloader")
+    await waitForResponse((request, reply) => request["type"] == "downloader", id)
 }
 
 let maxConcurrent = 3 // if we have too many going at once then printing takes longer than our hardcoded timout value
 
-let scrape = async (contents) => {
-    // await saveData("errors.txt", JSON.stringify(failure))
+let scrape = async (contents, history) => {
+
+    // store states
+    let failure = {}
+    let success = []
+    let currentlyRunning = {}
+
+    if (history) {
+        console.log("Entries before: ", contents.length)
+        contents = contents.filter(a => !history.some(b => a[0] == b))
+        console.log("Entries after: ", contents.length)
+    }
+
     scraping = true
 
     for (let i = 0; i < contents.length; i++) {
 
-        if (scraping == false) {
-            break
-        }
+        if (scraping == false) { break } // allow for user to stop scraping
 
         // if more that the maxConcurrent are running at once then wait for one to finish before continuing
-        if (currentlyRunning >= maxConcurrent) {
+        if (Object.keys(currentlyRunning).length >= maxConcurrent) {
+
             console.log("Reached capacity! Waiting for one to finish...")
+            await Promise.any(Object.values(currentlyRunning))
 
-            await new Promise((resolve, reject) => {
-                chrome.runtime.onMessage.addListener(function (request, sender, reply) {
-
-                    if (sender.tab && sender.tab.id && request["type"] == "error") {
-
-                        resolve()
-                        chrome.runtime.onMessage.removeListener(arguments.callee)
-                    }
-                })
-            })
+            console.log("Continue")
         }
-
-        currentlyRunning++
 
         let name = contents[i][0]
         let url = contents[i][1]
 
         console.log(name, url)
 
-        let id = (await chrome.tabs.create({ url: url })).id
+        try {
+            let id = (await chrome.tabs.create({ url: url })).id
 
-        console.log("waiting for status request")
-        await waitForResponse(id, (request, reply) => {
-            if (request["type"] == "scrape") {
-                reply(name)
-                return true
-            }
-            return false
-        })
-        console.log("and we're off!")
+            console.log("waiting for status request")
 
-        // page has been loaded, wait for download to finish - tab should report any errors
-        console.log("waiting for reply from", name) // this has been moved to the "master" listener
-    }
+            // jesus, javascript is funky - what the hell's the context here?! Even I don't get what I've written!
+            currentlyRunning[name] = new Promise(async (resolve, reject) => {
+                try {
 
-    console.log("waiting for all downloads to finish")
-    while (true) {
-        await getWait(100) // eh, hacky is my middle name
-        if (currentlyRunning == 0) {
-            break
+                    await waitForResponse((request, sender, reply) => {
+                        if (request["type"] == "scrape") {
+
+                            reply(name)
+                            return true
+                        }
+
+                        return false
+                    }, id)
+
+                    console.log("and we're off!")
+
+                    await waitForResponse((request, sender, reply) => { // store our promise in here but don't wait for it here
+
+                        if (request["type"] == "error") { // must listen for reply here to avoid race condition
+                            console.log("got reply from", request["name"])
+
+                            if (request["type"] == "error") {
+
+                                if (request["error"] && request["error"].length > 0) {
+                                    failure[request["name"]] = request["error"]
+                                    console.log("error: ", request["error"])
+
+                                    reply({ "received": true })
+
+                                } else {
+                                    success.push(request["name"])
+                                }
+                            }
+
+                            console.log("finished with ", request["name"])
+
+                            delete currentlyRunning[request["name"]] // finished!
+                            return true
+                        }
+                    }, id)
+
+                    resolve()
+
+                } catch (error) { reject(error) } // unfortunately two try catches needed because js problems (actually valid design faults on my part but let's blame js instead!)
+            })
+        } catch (error) {
+            console.log(`error: ${error}`)
+
+            failure[name] = `failed to load tab, error: ${error}`
+            delete currentlyRunning[request["name"]] // finished!
         }
     }
 
+    console.log("waiting for all downloads to finish")
+    await Promise.allSettled(Object.values(currentlyRunning))
+
     console.log(failure)
-    if (scraping) {
-        await saveData("errors.txt", JSON.stringify(failure))
-    } else {
-        await saveData("progress.txt", JSON.stringify(success)) // we don't need to track errors
-    }
+
+    await saveData("progress.txt", JSON.stringify(success)) // we don't need to track errors
+    await saveData("errors.txt", JSON.stringify(failure))
 
     console.log("WE FINISHED BOI!")
     chrome.runtime.reload() // much easier this way
